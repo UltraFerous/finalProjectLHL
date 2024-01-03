2cc13f591d6a4019af4c96227270df7326f67f36
dylanhamada <dylan.hamada@gmail.com>
2024-01-03 13:54:17 -0500

    Define Routes in Express Server
    - Create all route files in routes directory
    - Create all query helper files in queries directory
    - Import routes in Express index.js
    - Define route middleware in Express server
    - Create routes subdirectory in server directory
    - Create queries subdirectory in db directory

---
+++ server/routes/developers.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// import query helper functions and use them in routes
+
+// developers list page
+router.get('/', (req, res) => {
+
+});
+
+module.exports = router;
---
+++ server/routes/orgs.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// import query helper functions and use them in routes
+
+// organization details page
+router.get('/:id', (req, res) => {
+
+});
+
+// create organization page
+router.get('/create', (req, res) => {
+
+});
+
+module.exports = router;
