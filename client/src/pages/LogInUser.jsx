49b4d2aa64d54d5746d57c46274f915b89d82b3e
TiffanyStPierre <stpierre.tiffany@gmail.com>
2024-01-04 11:10:29 -0700

    set up UserContext file using context api

--- client/src/pages/LogInUser.jsx
+++ client/src/pages/LogInUser.jsx
@@ -1,4 +1,9 @@
+import { useContext } from 'react';
+import UserContext from '../context/UserContext';
+
 export default function LogInUser() {
+
+  const {loading, updateLoading, user, updateCurrentUser} = useContext(UserContext);
 
   return (
     <>
