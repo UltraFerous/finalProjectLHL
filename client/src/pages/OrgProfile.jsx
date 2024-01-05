8d5f36626d114d9944616a03f6f5080b312aa0f9
UltraFerous <jandoga31@gmail.com>
2024-01-05 17:10:33 -0500

    Fixed an issue where projectDataSearchID was getting org data and started org routes

--- client/src/pages/OrgProfile.jsx
+++ client/src/pages/OrgProfile.jsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function OrgProfile() {
 
   const { id } = useParams();
+  const [org, setOrg] = useState(null);
+
+  useEffect(() => {
+    const fetchOrgDetails = () => {
+      axios.get(`http://localhost:8080/org/${id}`)
+        .then((response) => {
+          const data = response.data;
+          setOrg(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching project details:', error);
+        });
+    };
+
+    fetchOrgDetails();
+  }, [id])
 
   return (
     <>
+      <h1>{org && org[0].name}</h1>
       <h1>Organization Details Page</h1>
       <h2>Details for Organization {id}</h2>
     </>
--- server/routes/orgs.js
+++ server/routes/orgs.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { orgDataSearchID } = require('../db/queries/orgs.js');
 
 // import query helper functions and use them in routes
 
 // organization details page
 router.get('/:id', (req, res) => {
+  const org_id = req.params.id;
 
+  orgDataSearchID(org_id)
+    .then((orgData) => {
+      console.log(orgData);
+
+      // Check if orgData exists
+      if (orgData.length > 0) {
+        res.status(200).json(orgData);
+      } else {
+        res.status(404).json({ error: 'Project not found' });
+      }
+    })
+    .catch((err) => {
+      console.error("ERROR:", err.message);
+      res.status(500).json({ error: 'Internal server error' });
+    });
 });
 
 // create organization page
