563922b06d3cc7b1331254530b851cbb9703a86d
dylanhamada <dylan.hamada@gmail.com>
2024-01-09 16:52:21 -0500

    Redesign UserProfile Component

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,41 +1,47 @@
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Image, Button } from "react-bootstrap";
 import userTwo from "../images/user-2.jpg";
 
 export default function UserProfile() {
 
   return (
     <>
-      <Container className="mt-5">
-        <Row className="text-center mb-5">
-          <h1>User Name</h1>
-        </Row>
-        <Row>
-          <Col xs={6}>
+      <Container className="my-5">
+        <Row className="d-flex justify-content-center">
+          <Col md={6} className="mx-auto">
+            <Row className="mb-4 text-center">
+              <h1 className="fw-semibold">User Name</h1>
+            </Row>
+            <Row className="mb-4">
+              <Col>
+                <Image
+                  src={userTwo}
+                  rounded
+                  fluid
+                />
+              </Col>
+            </Row>
+            <Row className="mb-4">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec gravida aliquam sem, id gravida odio pretium elementum. Nunc turpis purus, vulputate sed odio ac, pulvinar dapibus magna. Vestibulum at enim nec elit euismod imperdiet. Suspendisse et pretium lacus. Etiam lobortis rhoncus massa, nec vehicula mi gravida id. Duis scelerisque felis eu malesuada bibendum. Donec lorem massa, gravida vitae est et, mattis molestie sem. In eleifend accumsan sagittis. Aliquam vulputate scelerisque risus. Mauris non malesuada urna. Vivamus suscipit laoreet maximus. Etiam gravida massa sit amet tincidunt condimentum.</p>
+            </Row>
+            <Row>
+              <h5>Skills</h5>
+            </Row>
             <Row className="mb-5">
-              <img
-                src={userTwo}
-                alt="User"
-                className="proj-org-images"
-              />
+              <Col className="col-auto">
+                <Button size="sm">Skill</Button>
+              </Col>
+              <Col className="col-auto">
+                <Button size="sm">Skill</Button>
+              </Col>
+              <Col className="col-auto">
+                <Button size="sm">Skill</Button>
+              </Col>
+              <Col className="col-auto">
+                <Button size="sm">Skill</Button>
+              </Col>
             </Row>
             <Row>
-              <p>Morbi pharetra nulla sem, in vehicula enim viverra et. In ut pharetra felis, non condimentum quam. Aliquam vitae placerat quam. Sed lacus nulla, efficitur in nibh sollicitudin, suscipit consectetur magna. Nam erat dui, condimentum posuere libero sed, fringilla imperdiet nunc. Nunc non molestie odio, a fermentum magna. Integer bibendum hendrerit tristique. Praesent vestibulum eget enim in faucibus. Nullam eget sem hendrerit, tempus quam vitae, pellentesque nisl. Morbi efficitur ex in mauris aliquet, ac vestibulum est scelerisque. Aenean ullamcorper nibh nec massa pulvinar tempus vel in nunc.</p>
-            </Row>
-          </Col>
-          <Col>
-            <Row className="mb-2">
-              <h5>Location</h5>
-            </Row>
-            <Row>
-              <p>Halifax, Nova Scotia, Canada</p>
-            </Row>
-            <Row className="mb-2">
-              <h5>Contact User Name</h5>
-            </Row>
-            <Row>
-              <a href="#">
-                <Button variant="success">Email User Name</Button>
-              </a>
+              <h5>Projects</h5>
             </Row>
           </Col>
         </Row>
