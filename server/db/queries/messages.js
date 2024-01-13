7bcc550803a11ef506107ccee1479477ab9aadd7
dylanhamada <dylan.hamada@gmail.com>
2024-01-13 12:23:31 -0500

    Create SELECT Query for Direct Messages

---
+++ server/db/queries/messages.js
@@ -0,0 +1,21 @@
+const { db } = require('../connection');
+
+// read messages between two users organized chronologically
+const allMessages = function() {
+  return db
+    .query(`
+      SELECT * FROM direct_messages
+      WHERE (sender_id = $1 AND receiver_id = $2) OR (sender_id = $2 AND receiver_id = $2)
+      ORDER BY sent_at;
+    `, [userOne, userTwo])
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => {
+      console.log('ERROR:', err.message);
+    });
+};
+
+module.exports = {
+  allMessages
+};
