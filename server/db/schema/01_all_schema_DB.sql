fbbf6e8407d7e0d73310e88057c9bec0b928de3d
dylanhamada <dylan.hamada@gmail.com>
2024-01-13 10:37:21 -0500

    Create Direct Messages Table
    - Create direct_messages schema

--- server/db/schema/01_all_schema_DB.sql
+++ server/db/schema/01_all_schema_DB.sql
@@ -13,3 +13,4 @@
 \i db/schema/11_developer_information.sql
 \i db/schema/13_project_status_types.sql
 \i db/schema/14_assigned_tags_projects.sql
+\i db/schema/15_direct_messages.sql
---
+++ server/db/schema/15_direct_messages.sql
@@ -0,0 +1,7 @@
+CREATE TABLE direct_messages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  sender_id INTEGER REFERENCES users(id) NOT NULL,
+  receiver_id INTEGER REFERENCES users(id) NOT NULL,
+  message_text TEXT NOT NULL,
+  sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
